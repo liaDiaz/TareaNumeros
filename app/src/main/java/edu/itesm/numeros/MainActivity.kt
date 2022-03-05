60cbffbf29881a57636d977b2109d5a101fdc865
liaDiaz <46771468+liaDiaz@users.noreply.github.com>
2022-03-04 20:39:30 -0600

    commit

--- app/src/main/java/edu/itesm/numeros/MainActivity.kt
+++ app/src/main/java/edu/itesm/numeros/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setContentView(binding.root)
         //val es mutable
         //var no lo es
         val lista = mutableListOf<Int>()
